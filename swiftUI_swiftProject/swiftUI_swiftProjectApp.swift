d231fb66babcf560f8b49d7e2186f3e89d244bb1
SatoruKikuchi123 <satoru_kikuchi@mail.toyota.co.jp>
2023-06-12 17:19:00 +0900

    Initial Commit

---
+++ swiftUI_swiftProject/swiftUI_swiftProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_swiftProjectApp.swift
+//  swiftUI_swiftProject
+//
+//  Created by user on 2023/06/12.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_swiftProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
